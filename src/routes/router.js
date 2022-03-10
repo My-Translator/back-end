659557dee138713024b7511b16404b73e964b2a9
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2022-03-10 21:48:18 +0200

    update word

--- src/routes/router.js
+++ src/routes/router.js
@@ -36,4 +36,29 @@ myRouter.get('/getAllWords', async (req, res, next) => {
     res.status(200).json(sortedRecord);
 })
 
+myRouter.put("/updateWord", async (req, res, next) => {
+    let word = {
+        word: req.body.word,
+        translation: req.body.translation,
+    }
+    await Word.update(word, {
+        where: {
+            id: req.body.id 
+        }
+    });
+
+    let record = await Word.findAll();
+    let sortedRecord = record.sort((a, b) => {
+        let wordA = a.word.toLowerCase();
+        let WordB = b.word.toLowerCase();
+        if (wordA < WordB) //sort string ascending
+            return -1;
+        if (wordA > WordB)
+            return 1;
+        return 0; //default return value (no sorting)
+    });
+
+    res.status(200).json(sortedRecord);
+})
+
 module.exports = myRouter;
