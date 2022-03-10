d071e79c41896a65c97602eb98094b2b0c2fff04
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2022-03-10 18:17:16 +0200

    update

--- src/routes/router.js
+++ src/routes/router.js
@@ -7,8 +7,18 @@ const { Word } = require('../models/index');
 
 myRouter.post('/newWord', async (req, res, next) => {
     await Word.create(req.body);
-    res.status(201).json("Added successfully");
+    let record = await Word.findAll();
+    let sortedRecord = record.sort((a, b) => {
+        let wordA = a.word.toLowerCase();
+        let WordB = b.word.toLowerCase();
+        if (wordA < WordB) //sort string ascending
+            return -1;
+        if (wordA > WordB)
+            return 1;
+        return 0; //default return value (no sorting)
+    });
 
+    res.status(201).json(sortedRecord);
 });
 
 myRouter.get('/getAllWords', async (req, res, next) => {
